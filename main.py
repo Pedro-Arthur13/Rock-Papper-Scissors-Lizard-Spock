7b2e65f6e35c03ecbc35a2d284336aa9833336eb
Pedro-Arthur13 <parthurmalbuquerque@gmail.com>
2025-03-04 20:44:59 -0300

    Judge

--- main.py
+++ main.py
@@ -1,12 +1,13 @@
 import os
+import random
 
 class InvalidOption(Exception):
     """
         Excetion for invalid option
     """
     def __init__(self,message=f"""
-        The selected option is invalid
-        A opção selecionada é inválida
+                The selected option is invalid
+                A opção selecionada é inválida
         """):
         self.message = message
     def __str__(self):
@@ -77,8 +78,8 @@ def play():
                 Lagarto - 4
                 Spock - 5
         """)
-        move = int(input("""
-                """))
+        # move = int(input("""
+        #         """))
     else:
         print("""
             Make Your Move:
@@ -117,18 +118,73 @@ def get_option():
         option = int(input("""
                 """))
     return option
-    
 
-language = None
 
-while True:
-    move = None
-    option = None
-    if language is None:
+def judge(player):
+    # Antes de mostar a jogada do bot fazer um contdown até 3 mostrando na tela de maneira grande
+    # Para mostar na tela trazer uma arte ASCII assim https://www.reddit.com/r/Python/comments/y6gn1g/rock_paper_scissors_game_in_the_terminal_with/?rdt=41443 
+    if language == 1:
+        plays = {
+            1:"Rock",
+            2:"Papper",
+            3:"Scissors",
+            4:"Lizard",
+            5:"Spock"
+        }
+    else:
+        plays = {
+            1:"Pedra",
+            2:"Papel",
+            3:"Tesoura",
+            4:"Lagarto",
+            5:"Spock"
+        }
+
+    bot_play = language[random.randrange(1,6)]
+
+    if bot_play == player:
+        print("Empate!")
+        #return
+
+    elif (bot_play == 3 and player == 2) or   \
+         (bot_play == 2 and player == 1) or   \
+         (bot_play == 1 and player == 4) or   \
+         (bot_play == 4 and player == 5) or   \
+         (bot_play == 5 and player == 3) or   \
+         (bot_play == 3 and player == 4) or   \
+         (bot_play == 4 and player == 2) or   \
+         (bot_play == 2 and player == 5) or   \
+         (bot_play == 5 and player == 1) or   \
+         (bot_play == 1 and player == 3):      
+        
+        print("\033[91mYOU LOSE!\033[0m")  # Texto vermelho
+
+        """
+            Jogadas do ELIF:
+
+                1 .Tesoura corta papel
+                2 .Papel cobre pedra
+                3 .Pedra esmaga lagarto
+                4 .Lagarto envenena Spock
+                5 .Spock esmaga tesoura
+                6 .Tesoura decapita lagarto
+                7 .Lagarto come papel
+                8 .Papel refuta Spock
+                9 .Spock vaporiza pedra
+            10 .Pedra esmaga tesoura
+        """
+
+    else:
+        print("\033[92mYOU WIN!\033[0m")   # Texto verde
+
+    continuer = input("")
+
+
+def select_language():
         print(""" 
-                    Pick Up a Language (1 - US)
-                    Escolha um Idioma (2 - PT-BR)    
-        """)
+                Pick Up a Language (1 - US)
+                Escolha um Idioma (2 - PT-BR)    
+            """)
         try:
             language = int(input(""))
             if language < 1 or language > 2:
@@ -136,11 +192,29 @@ while True:
         except InvalidOption as e:
             os.system("clear")
             print(f"{str(e)}")
-            language = None
-            continue
+            continuer = input("""
+                Press ENTER to continue""" if language == 1 else
+                """
+                Pressione ENTER para continuar
+                """)
+            return None
         except ValueError:
             os.system("clear")
             print(""" Tente Novamente """)
+            return None
+
+        return language
+
+language = None
+player_wins = False
+
+while True:
+    move = None
+    option = None
+    if language is None:
+        os.system("clear")
+        language = select_language()
+        if language is None:
             continue
     
     
@@ -182,4 +256,5 @@ while True:
             continue
 
     move = play()
-    print(move)
+    judge(move)
+    # print(move)
